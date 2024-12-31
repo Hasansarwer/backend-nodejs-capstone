a6f4fdef055e0a11aaf2a532ed70078dc713b6fe
Hasansarwer <hasanmathju@gmail.com>
2024-12-31 14:25:39 +0600

    login completed

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -5,6 +5,7 @@ const connectToDatabase = require('../models/db');
 const router = express.Router();
 const dotenv = require('dotenv');
 const pino = require('pino');  // Import Pino logger
+const e = require('express');
 dotenv.config();
 
 const logger = pino();  // Create a Pino logger instance
@@ -47,6 +48,43 @@ router.post('/register', async (req, res) => {
     } catch (e) {
         logger.error(e);
         return res.status(500).send('Internal server error');
+    }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        // Task 1: Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
+        const db = await connectToDatabase();
+        // Task 2: Access MongoDB `users` collection
+        const collection = db.collection("users");
+        // Task 3: Check for user credentials in database
+        const user = await collection.findOne({ email: req.body.email });
+        // Task 4: Check if the password matches the encrypted password and send appropriate message on mismatch
+        if (user) {
+            const result = await bcryptjs.compare(req.body.password, user.password);
+            if (!result) {
+                logger.error('Passwords do not match');
+                return res.status(400).send('Wrong pasword');
+            }
+            // Task 5: Fetch user details from a database
+            const userName = user.firstName;
+            const userEmail = user.email;
+            // Task 6: Create JWT authentication if passwords match with user._id as payload
+            const payload = {
+                user: {
+                    id: user._id,
+                },
+            };
+            const authtoken = jwt.sign(payload, JWT_SECRET);
+            res.json({authtoken, userName, userEmail });
+        }else{
+            // Task 7: Send appropriate message if the user is not found
+            logger.error('User not found');
+            return res.status(400).send('User not found');
+        }
+    } catch (e) {
+         return res.status(500).send('Internal server error');
+
     }
 });
 
