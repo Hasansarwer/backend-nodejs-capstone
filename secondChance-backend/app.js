da8da58d4e9057de8cb4a57a7951684162372ea0
Hasansarwer <hasanmathju@gmail.com>
2024-12-31 19:29:07 +0600

    image path fixed

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -56,7 +56,7 @@ app.use('/api/auth', authRoutes);
 
 
 
-app.use('/images', path.join(__dirname,express.static('public/images')));
+app.use('/images', express.static(path.join(__dirname,'public/images')));
 // Global Error Handler
 app.use((err, req, res, next) => {
     console.error(err);
